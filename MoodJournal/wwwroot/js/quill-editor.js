24e85226b557cf5ad433fbe5699e98dbc436aa94
shrine-09 <shrineghimire36@gmail.com>
2026-01-24 17:05:19 +0545

    Fixed Rich Text Issues

--- MoodJournal/wwwroot/js/quill-editor.js
+++ MoodJournal/wwwroot/js/quill-editor.js
@@ -1,10 +1,37 @@
-var quill;
+var quill = null;
 
 window.initQuill = function () {
-    var el = document.getElementById("quillEditor");
-    if (!el) return;
+    var host = document.getElementById("quillEditor");
+    if (!host) return;
 
-    quill = new Quill("#quillEditor", { theme: "snow" });
+    if (host.__quill) {
+        quill = host.__quill;
+        return;
+    }
+
+    var wrapper = host.closest(".editor-card") || host.parentElement;
+    if (wrapper) {
+        wrapper.querySelectorAll(".ql-toolbar").forEach(function (t) { t.remove(); });
+    }
+
+    var clean = host.cloneNode(false);
+    clean.innerHTML = "";
+    host.parentNode.replaceChild(clean, host);
+
+    var toolbar = [
+        [{ header: 1 }, { header: 2 }, { header: 3 }, { header: false }],
+        ["bold", "italic", "underline", "strike"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        [{ align: [] }],
+        ["blockquote", "code-block", "link"],
+        ["clean"]
+    ];
+
+    quill = new Quill(clean, {
+        theme: "snow",
+        modules: { toolbar: toolbar }
+    });
 };
 
 window.getQuillContent = function () {
